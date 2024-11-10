18f7cbdcc31fab21226fa44da1b171388933e0d1
Amel Ramirez <amelrc@yahoo.com>
2024-11-10 16:14:54 +0100

    added GA

--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@
 // );
 
 import * as React from "react";
+import ReactGA from "react-ga4";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App";
+
+ReactGA.initialize("465678130");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const root = createRoot(document.getElementById("root"));
 
